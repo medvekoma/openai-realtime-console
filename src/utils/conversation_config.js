5f4cc44d47128659a7ca4c4d45fb2ba9c6aab041
Attila Szucs <attila.szucs@gmail.com>
2025-03-18 06:46:19 +0100

    minor change

--- src/utils/conversation_config.js
+++ src/utils/conversation_config.js
@@ -1,22 +1,21 @@
 export const it_night_program = {
     "program": [
         {
-            "time": "18:00",
-            "speaker": "",
+            "time": "6:00pm",
             "title": "Welcome Speech",
             "tags": ["AI", "Technologies"],
             "stage": "Tech Horizon Stage"
         },
         {
-            "time": "18:10",
+            "time": "6:10pm",
             "speaker": "Maryna Didkovska, Richard Oliver Legendi",
             "title": "EPAM Roundtable: Leadership, Diversity and Success",
             "abstract": "Join us at the EPAM Roundtable: Leadership, Diversity, and Success for an engaging and uplifting discussion with three of EPAM's leaders! Hear their inspiring stories about how diversity shaped their careers and gather valuable tips to enhance your own career in a supportive, inclusive environment. It's a great opportunity to connect, learn, and feel inspired. We can’t wait to see you there!",
-            "tags": ["AI", "Career Advising", "Career well-being", "Data", "Leadership", "Service Delivery", "Technologies", "Testing"],
+            "tags": ["AI", "Career", "Leadership", "Technologies"],
             "stage": "Tech Horizon Stage"
         },
         {
-            "time": "19:05",
+            "time": "7:05pm",
             "speaker": "Attila Szűcs",
             "title": "Beyond Text: When AI Finds Its Voice",
             "abstract": "Text-based chatbots have become part of our daily lives, but human communication is rich with voice, tone, and emotion. Join me to explore how we can create voice-based chatbots that sound natural and connect with users on a more human level.",
@@ -24,7 +23,7 @@ export const it_night_program = {
             "stage": "Tech Horizon Stage"
         },
         {
-            "time": "19:05",
+            "time": "7:05pm",
             "speaker": "Marton Soltesz-Nagy, Adam Revesz",
             "title": "EPAM Campus Tech Talk: Navigating Careers in Automated Testing, DevOps and Front-End",
             "abstract": "Are you about to launch your career in tech? Don't miss this roundtable discussion at the Insight & Innovation Stage, where three experienced professionals will share about their journeys in DevOps, Front-End, and Automated Testing. They will highlight key milestones and lessons learned throughout their careers. Gain practical tips for entering the IT field and learn about the opportunities available through EPAM Campus programs. See you there!",
@@ -32,7 +31,7 @@ export const it_night_program = {
             "stage": "Insight & Innovation Stage"
         },
         {
-            "time": "19:05",
+            "time": "7:05pm",
             "speaker": "Ronald David, Giovanna Pimenta",
             "title": "Mastering the Art of Communication",
             "abstract": "Discover the transformative power of communication. This engaging session will explore key strategies and best practices for enhancing communication, negotiation, and persuasion skills that go beyond technical expertise. Dive into real-life applications that reveal how subtle shifts in your communication approach can lead to more successful outcomes in any situation.",
@@ -40,14 +39,14 @@ export const it_night_program = {
             "stage": "Visionary Voices Stage"
         },
         {
-            "time": "19:35",
+            "time": "7:35pm",
             "speaker": "Maryna Didkovska",
             "title": "AI: Breaking through resistance and fears",
             "tags": ["Technologies", "Testing"],
             "stage": "Tech Horizon Stage"
         },
         {
-            "time": "20:20",
+            "time": "8:20pm",
             "speaker": "Oresztesz Margaritisz",
             "title": "How to build a modular retro arcade?",
             "abstract": "A regular arcade cabinet won't fit into a normal household. This talk will give you some tips on how can you still make it at home with sensible compromises. You can even try out the end product for yourself.",
@@ -55,7 +54,7 @@ export const it_night_program = {
             "stage": "Insight & Innovation Stage"
         },
         {
-            "time": "20:35",
+            "time": "8:35pm",
             "speaker": "Sandor Zsolt Ando",
             "title": "Self-made influencers, gen-z leaders",
             "abstract": "Who are Gen-Z leaders? Where are they? How will they shape the industry?",
@@ -63,7 +62,7 @@ export const it_night_program = {
             "stage": "Visionary Voices Stage"
         },
         {
-            "time": "20:35",
+            "time": "8:35pm",
             "speaker": "Katalin Kiss",
             "title": "Discover your enterprise: Boosting data accuracy through domain context",
             "abstract": "Leverage enterprise analysis, business architecture perpectives for the sake of unified design patterns and business domain management. Join me to see how you may accelerate data processing and analysis.",
@@ -71,7 +70,7 @@ export const it_night_program = {
             "stage": "Tech Horizon Stage"
         },
         {
-            "time": "20:50",
+            "time": "8:50pm",
             "speaker": "Gergely Papp",
             "title": "Growing pains: From Startup to Enterprise",
             "abstract": "Join us for an exploration of how businesses evolve from startups to complex enterprises, and the evolution of the challenges they face along the way.",
@@ -79,7 +78,7 @@ export const it_night_program = {
             "stage": "Insight & Innovation Stage"
         },
         {
-            "time": "21:05",
+            "time": "9:05pm",
             "speaker": "Adam Gonda",
             "title": "npm i ai",
             "abstract": "How to add AI features to your app effortlessly into your javascript project.",
@@ -87,8 +86,7 @@ export const it_night_program = {
             "stage": "Tech Horizon Stage"
         },
         {
-            "time": "21:35",
-            "speaker": "",
+            "time": "9:35pm",
             "title": "Networking",
         }
     ]
@@ -116,5 +114,5 @@ Instructions:
 Personality:
 - Be upbeat and genuine
 - Try speaking quickly as if excited
-- Try to pronounce the hungarian names in hungarian.
+- Try to pronounce the Hungarian names in Hungarian.
 `;
